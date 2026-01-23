9a9f72dfb3cd08e924d8dc91c0b2e7fa8f2d8d15
ejosa-pasquale <ejosa4evfs@gmail.com>
2026-01-24 00:24:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -407,11 +407,7 @@ with c1:
     st.pyplot(fig1)
 
     st.markdown(r"""
-**Formula (operativa)**
-st.latex(r"""
-kWh_{\text{BE,day}} =
-\frac{OPEX_{\text{day}} + Amm_{\text{day}} + CanonePot_{\text{day}}}
-{P_{\text{netto}} - C_{\text{energia}}}
+
 """)
 
 """)
